1bf14a835a791d22b9e468604f65821694278651
Daniel Moura <daniel.mouraa@gmail.com>
2019-05-30 14:13:31 -0300

    [CONFLITO]

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -12,6 +12,18 @@
 			this.scrollToAnchor();
 			this.multimedia();
 			this.carouselAnexos();
+
+
+	  $('.tab a').on('click', function (e) {
+	  e.preventDefault();
+	  
+	  $(this).parent().addClass('active');
+	  $(this).parent().siblings().removeClass('active');
+	  
+	  var href = $(this).attr('href');
+	  $('.forms > form').hide();
+	  $(href).fadeIn(500);
+	});
 		},
 
 		/**
