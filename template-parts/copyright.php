b78735753bb8b59f0b433b1dfd4945270c8d3eab
Ricardo Carvalho <rickmanu@gmail.com>
2019-06-06 09:50:39 -0300

    [UPDATE] Listagem de posts com imagem destacada

--- template-parts/copyright.php
+++
@@ -1,4 +0,0 @@
-<div class="texto-copyright align-items-center pt-3 pb-3 d-flex mt-4 mb-4">
-  <span>Todo o conteúdo deste site está publicado sob a Licença <a rel="license" target="_blank" href="https://creativecommons.org/licenses/by-nd/3.0/deed.pt_BR">Creative Commons Atribuição-SemDerivações 3.0 Não Adaptada</a>.</span>
-  <div class="voltar-topo"><a href="#main-single">Voltar ao Topo</a></div>
-</div>
